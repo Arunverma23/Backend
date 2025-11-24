097d718fde7e4b3aa720c0e49bf2a3c3a32bf3b0
Arun Verma <info.arunverma23@gmail.com>
2025-11-24 16:10:10 +0530

    Node Apllication

---
+++ 00_demo/index.js
@@ -0,0 +1,27 @@
+// require('dotenv').config();
+const express = require('express')
+
+// import express from "express"
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) => {
+    res.send("hello twitter")
+})
+
+app.get('/login', (req,res) => {
+    res.send('<h1> login page </h1>')
+})
+
+app.get('/youtube', (req,res) => {
+    res.send('<h2>Arun</h2>')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
