0b40f5cb237db0286e5d5de86fc08afe4c5d374c
Arun Verma <info.arunverma23@gmail.com>
2025-11-26 02:02:38 +0530

    util code explain

---
+++ BackEndProject/src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+// Above example is From Promises
+
+export {asyncHandler}
+
+
+// Try Catch Example
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
