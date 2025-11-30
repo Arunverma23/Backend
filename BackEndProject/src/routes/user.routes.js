4bd0b5cec3ba790720758431071a04ef3fb5502d
Arun Verma <info.arunverma23@gmail.com>
2025-12-01 03:08:50 +0530

    auth middleware

--- BackEndProject/src/routes/user.routes.js
+++ BackEndProject/src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, registerUser } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { logoutUser } from "../controllers/user.controllers.js";
 
 const router = Router()
 
@@ -17,5 +19,9 @@ router.route("/register").post(
     ]),
     registerUser)
 
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
